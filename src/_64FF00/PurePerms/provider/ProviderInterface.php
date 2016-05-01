1fe695f51dce79ba379502b4dc199d2644511fa4
Muqsit Rayyan <hackhack05@gmail.com>
2016-05-01 17:59:55 +0300

    Create ProviderInterface.php

---
+++ src/_64FF00/PurePerms/provider/ProviderInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace _64FF00\PurePerms\provider;
+
+use _64FF00\PurePerms\PPGroup;
+
+use pocketmine\IPlayer;
+
+interface ProviderInterface
+{
+
+    public function getGroupData(PPGroup $group);
+
+    public function getGroupsData();
+
+    public function getPlayerData(IPlayer $player);
+
+    public function setGroupData(PPGroup $group, array $tempGroupData);
+
+    public function setGroupsData(array $tempGroupsData);
+
+    public function setPlayerData(IPlayer $player, array $tempPlayerData);
+
+    public function close();
+}
